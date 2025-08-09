5e50a7507571a6748caf4b7ca447998ba5d8c14c
thesachin26 <codesachin.26@gmail.com>
2025-08-09 08:30:24 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,3 @@
+s1 = input("Enter your first name ")
+s2 = input("Enter your second name ")
+print("Hello",s1,s2, "! welcome to Python Program")
---
+++ mathematical expression.py
@@ -0,0 +1,15 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+a= int (a)
+b = int(b)
+
+addition = a+b
+subtraction = a-b
+division = a/b
+multiplication = a*b
+
+print("Addition",addition)
+print("Subtraction",subtraction)
+print("division",division)
+print("multiplication",multiplication)
